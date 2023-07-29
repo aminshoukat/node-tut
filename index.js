01de5068d6345970288cb0de2b0382da1dedd26c
Amin Shoukat <aminshoukat4@gmail.com>
2023-07-30 02:12:47 +0500

    Create Server Using Node

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 // let countries = require('./countries');
-let fs = require('fs');
+// let fs = require('fs');
+// fs.writeFileSync('new.txt', 'Hello World');
+
+let http = require('http');
 
 
-fs.writeFileSync('new.txt', 'Hello World');
+http.createServer( (req,resp) => {
+
+    resp.write('Hello World from Node JS');
+    resp.end();
+
+}).listen(4500)
