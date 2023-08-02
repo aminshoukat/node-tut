76eaf429b78b8eb72dab0062cd65faa47175f6f4
Amin Shoukat <aminshoukat4@gmail.com>
2023-08-03 00:06:00 +0500

    Installed express js, create get routes with listen

--- index.js
+++ index.js
@@ -1,46 +1,11 @@
-// let countries = require('./countries');
-// let fs = require('fs');
-// let path = require('path');
-// let pathDir = path.join(__dirname, 'files');
-// let http = require('http');
+const express = require('express');
+const app = express();
 
+app.get('', (req, resp) => {
+    resp.send('This is homepage.');
+});
+app.get('/about-us', (req, resp) => {
+    resp.send('This is about us page.');
+});
 
-
-// fs.writeFileSync('new.txt', 'Hello World');
-
-// http.createServer( (req,resp) => {
-//     resp.writeHead(200, {'Content-Type':'application/json'});
-//     resp.write(JSON.stringify(countries));
-//     resp.end();
-
-// }).listen(4500);
-
-
-// for (let i = 1; i < 6; i++) {
-//     fs.writeFileSync(`${pathDir}/file_${i}.txt`, `I love Pakistan ${i}`);
-// }
-
-// fs.readdir(pathDir, (err, files) => {
-//     files.forEach(file => {
-//         console.log(file);
-//     });
-// });
-
-
-let a = 1;
-let b = 2;
-
-let waitingData = new Promise((resolve, reject) => {
-
-    setTimeout(() => {
-        resolve(2);
-    }, 2000);
-
-})
-
-waitingData.then((data) => {
-
-    a = data;
-    console.log(a + b);
-})
-
+app.listen(5000);
