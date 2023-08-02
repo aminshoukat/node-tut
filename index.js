030dc5d0708e5e8944194744e5ed494b51190d64
Amin Shoukat <aminshoukat4@gmail.com>
2023-08-02 23:47:36 +0500

    learn js Promise

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-let countries = require('./countries');
-let fs = require('fs');
-let path = require('path');
-let pathDir = path.join(__dirname, 'files');
+// let countries = require('./countries');
+// let fs = require('fs');
+// let path = require('path');
+// let pathDir = path.join(__dirname, 'files');
 // let http = require('http');
 
 
@@ -16,12 +16,31 @@ let pathDir = path.join(__dirname, 'files');
 // }).listen(4500);
 
 
-for (let i = 1; i < 6; i++) {
-    fs.writeFileSync(`${pathDir}/file_${i}.txt`, `I love Pakistan ${i}`);
-}
+// for (let i = 1; i < 6; i++) {
+//     fs.writeFileSync(`${pathDir}/file_${i}.txt`, `I love Pakistan ${i}`);
+// }
 
-fs.readdir(pathDir, (err, files) => {
-    files.forEach(file => {
-        console.log(file);
-    });
-});
+// fs.readdir(pathDir, (err, files) => {
+//     files.forEach(file => {
+//         console.log(file);
+//     });
+// });
+
+
+let a = 1;
+let b = 2;
+
+let waitingData = new Promise((resolve, reject) => {
+
+    setTimeout(() => {
+        resolve(2);
+    }, 2000);
+
+})
+
+waitingData.then((data) => {
+
+    a = data;
+    console.log(a + b);
+})
+
