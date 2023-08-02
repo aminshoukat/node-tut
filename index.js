7c02386331910bd306f8dab9bf588e79a798d78b
Amin Shoukat <aminshoukat4@gmail.com>
2023-08-03 00:45:00 +0500

    Load static html pages

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 const express = require('express');
+const path = require('path');
 const app = express();
+const publicPath = path.join(__dirname, 'public');
 
-app.get('', (req, resp) => {
-    resp.send('This is homepage.');
-});
-app.get('/about-us', (req, resp) => {
-    resp.send('This is about us page.');
-});
+
+app.use(express.static(publicPath));
 
 app.listen(5000);
