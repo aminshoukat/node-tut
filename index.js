be8b1c57b39db0c6102769adf6a2547860efbc48
Amin Shoukat <aminshoukat4@gmail.com>
2023-08-03 01:48:54 +0500

    Remove extension from URL and 404 Page

--- index.js
+++ index.js
@@ -3,7 +3,16 @@ const path = require('path');
 const app = express();
 const publicPath = path.join(__dirname, 'public');
 
+// app.use(express.static(publicPath));
 
-app.use(express.static(publicPath));
+app.get('', (req, resp) => {
+    resp.sendFile(`${publicPath}/index.html`);
+});
+app.get('/about', (req, resp) => {
+    resp.sendFile(`${publicPath}/about.html`);
+});
+app.get('*', (req, resp) => {
+    resp.sendFile(`${publicPath}/404.html`);
+});
 
 app.listen(5000);
