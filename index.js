d60b5609cdd4d5f1b748a2a70515d1d57d61fa3e
Amin Shoukat <aminshoukat4@gmail.com>
2023-08-05 01:32:59 +0500

    Template Engine EJS used with dynamic data

--- index.js
+++ index.js
@@ -5,14 +5,27 @@ const publicPath = path.join(__dirname, 'public');
 
 // app.use(express.static(publicPath));
 
+// app.get('', (req, resp) => {
+//     resp.sendFile(`${publicPath}/index.html`);
+// });
+// app.get('/about', (req, resp) => {
+//     resp.sendFile(`${publicPath}/about.html`);
+// });
+// app.get('*', (req, resp) => {
+//     resp.sendFile(`${publicPath}/404.html`);
+// });
+
+app.set('view engine', 'ejs');
+
 app.get('', (req, resp) => {
-    resp.sendFile(`${publicPath}/index.html`);
-});
-app.get('/about', (req, resp) => {
-    resp.sendFile(`${publicPath}/about.html`);
-});
-app.get('*', (req, resp) => {
-    resp.sendFile(`${publicPath}/404.html`);
+    const user = {
+        "name": "Amin",
+        "father": "Shoukat",
+        "age": "30",
+        "nationality": "Pakistani",
+        "skills": ["HTML 5", "CSS 3", "JS", "jQuery", "PHP", "Node", "MYSQL"]
+    }
+    resp.render('home', {user} );
 });
 
 app.listen(5000);
